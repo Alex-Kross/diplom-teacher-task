3f3d29c68b2ccb64d321456e2bc4ebb2e897feaf
Alex-Kross <artemkirlanovia@gmail.com>
2023-06-16 03:53:14 +0300

    delete unusage file

--- src/main/java/com/bntu/diplom/teacherTask/repositories/TemplateFileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bntu.diplom.teacherTask.repositories;
-
-import com.bntu.diplom.teacherTask.models.TeacherFile;
-import com.bntu.diplom.teacherTask.models.TemplateFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//public interface TemplateFileRepository extends JpaRepository<TemplateFile, Long> {
-//}
